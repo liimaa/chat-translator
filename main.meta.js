b39bb8d0e76e39c6002311a1470cf527923b2890
Eemeli Sipiläinen <eemelizi1@outlook.com>
2019-07-21 22:44:38 +0300

    added @updateURL, @downloadURL, @supportURL, changed file name to match script update & download urls

---
+++ main.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         liima's chat translate
+// @namespace    chat-translator
+// @version      1.0.0
+// @description  translates chats
+// @author       liima
+// @license      MIT; https://opensource.org/licenses/MIT
+// @match        https://www.twitch.tv/*
+// @match        https://www.youtube.com/watch?*
+// @match        https://www.smashcast.tv/*
+// @match        https://mixer.com/*
+// @match        http://www.bigo.tv/*
+// @match        play.afreecatv.com/*
+// @match        https://www.huya.com/*
+// @match        https://instagib.tv/*
+// @match        https://live.bilibili.com/*
+// @match        https://*.nicovideo.jp/*
+// @match        https://sketch.pixiv.net/@*
+// @match        https://www.periscope.tv/w/*
+// @supportURL   https://github.com/liimaa/chat-translator/issues
+// @updateURL    https://raw.githubusercontent.com/liimaa/chat-translator/master/main.meta.js
+// @downloadURL  https://raw.githubusercontent.com/liimaa/chat-translator/master/main.user.js
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwwAADsMBx2+oZAAAAFRJREFUOE/NjEEOwCAMw/j/pzskRx1NuTBxmE+NSRhxyK8HoyLb0MO2hEwk84E8IQIRDxtlfBmshdsDa080MJt0r7xu7OZI3kwPpOoY/IPO8aAQ8QAVLnamZKyidwAAAABJRU5ErkJggg==
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
